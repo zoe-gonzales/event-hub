8a4c6c82579989c2c255ec473cee369d9d10eee0
Zoe Gonzales <zoegonzales03@gmail.com>
2023-04-09 18:00:21 -0600

    ensure db is connected to client

---
+++ app/models/event.server.ts
@@ -0,0 +1,14 @@
+import { prisma } from "~/db.server";
+
+export async function getEvents() {
+  return prisma.event.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      start: true,
+      end: true,
+      location: true,
+    },
+  });
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,37 +1,44 @@
-import type { V2_MetaFunction } from "@remix-run/react";
+import { json, type LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData, type V2_MetaFunction } from "@remix-run/react";
+import { getEvents } from "../models/event.server";
+
+type Event = {
+  id: string;
+  title: string;
+  description: string;
+  start: string;
+  end: string;
+  location: string;
+};
+
+type LoaderData = {
+  events: Awaited<Event[]>;
+};
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
+export const loader: LoaderFunction = async () => {
+  const events = await getEvents();
+  return json({ events });
+};
+
 export default function Index() {
+  const { events } = useLoaderData() as LoaderData;
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {events.map((event) => (
+          <li key={event.id}>
+            <Link to={`events/${event.id}`}>{event.title}</Link>
+            <p>{event.description}</p>
+            <p>{event.location}</p>
+            <p>{event.start}</p>
+            <p>{event.end}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
